f3d00b66b3a3f28d7794952bbb533d870f738b39
Peter Lehmann <ple@miracleas.dk>
2014-03-11 07:54:29 +0100

    use a waitfor and only master db

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -1,23 +1,22 @@
-﻿using System;
-using System.Data.SqlClient;
-using System.Transactions;
-
-namespace ConsoleApplication3
+﻿namespace ConsoleApplication3
 {
-    using System.Data;
+    using System;
+    using System.Data.SqlClient;
     using System.Diagnostics;
     using System.Threading;
+    using System.Transactions;
     using log4net;
     using log4net.Config;
+    using IsolationLevel = System.Data.IsolationLevel;
 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             XmlConfigurator.Configure();
 
             Run();
-            Run();
+            Run(); //SqlException: Distributed transaction completed. Either enlist this session in a new transaction or the NULL transaction.
             Run();
             Console.ReadLine();
         }
@@ -33,21 +32,21 @@ namespace ConsoleApplication3
                                                                                                  .IsolationLevel
                                                                                                  .Serializable,
                                                                                              Timeout =
-                                                                                                 TimeSpan.FromMinutes(10)
+                                                                                                 TimeSpan.FromMinutes(1)
                                                                                          }))
                 {
                     Transaction.Current.EnlistDurable(DummyEnlistmentNotification.Id, 
                         new DummyEnlistmentNotification(),
-                        EnlistmentOptions.EnlistDuringPrepareRequired);
+                        EnlistmentOptions.EnlistDuringPrepareRequired); // only to make sure the transcation promotes to DTC
                     using (
                         var con1 =
                             new SqlConnection(
-                                "Data Source=.;Initial Catalog=trio;Integrated Security=True;max pool size = 2"))
+                                "Data Source=.;Initial Catalog=master;Integrated Security=True;max pool size = 2"))
 
                     using (
                         var con2 =
                             new SqlConnection(
-                                "Data Source=.;Initial Catalog=master;Integrated Security=True;max pool size = 2")
+                                "Data Source=.;Initial Catalog=master;Integrated Security=True;max pool size = 2;min pool size=1")
                         )
                     {
                         con1.Open();
@@ -56,31 +55,8 @@ namespace ConsoleApplication3
                         using (var tx2 = con2.BeginTransaction(IsolationLevel.Serializable))
                         {
                             var cmd = con1.CreateCommand();
-                            cmd.CommandText = @"INSERT INTO [dbo].[CommonCodeDomain]
-           ([CommonCodeDomainId]
-           ,[DomainName]
-           ,[Note]
-           ,[CreatedDateTime]
-           ,[CreatedBy]
-           ,[ModifiedDateTime]
-           ,[ModifiedBy]
-           ,[Version])
-     VALUES
-           (@p1
-           ,@p2
-           ,@p3
-           ,getdate()
-           ,'user'
-           ,getdate()
-           ,'user'
-           ,1)";
-                            cmd.CommandType = CommandType.Text;
-                            cmd.Prepare();
-
-
-                            SetPar(cmd, "p1", new Guid("266424F8-0D04-46EE-B632-9C3552A6B649"));
-                            SetPar(cmd, "p2", "test");
-                            SetPar(cmd, "p3", "42");
+                            cmd.CommandTimeout = 30;
+                            cmd.CommandText = @"waitfor delay '00:02:00'"; //get a timeout from sql server and now my SqlConnection is dead
                             var res = cmd.ExecuteNonQuery();
                             tx1.Commit();
                             tx2.Commit();
@@ -98,14 +74,6 @@ namespace ConsoleApplication3
                 Console.WriteLine(e);
             }
         }
-
-        private static void SetPar(SqlCommand cmd, string name, object value)
-        {
-            var p = cmd.CreateParameter();
-            p.ParameterName = name;
-            p.Value = value;
-            cmd.Parameters.Add(p);
-        }
     }
 
     public class DummyEnlistmentNotification : IEnlistmentNotification
@@ -115,14 +83,14 @@ namespace ConsoleApplication3
         public void Prepare(PreparingEnlistment preparingEnlistment)
         {
             logger.DebugFormat("{0} {1} gettting call prepare", Thread.CurrentThread.Name, Thread.CurrentThread.ManagedThreadId);
-            Thread.Sleep(500);
+            //Thread.Sleep(500);
             preparingEnlistment.Prepared();
 
         }
 
         public void Commit(Enlistment enlistment)
         {
-            Thread.Sleep(500);
+            //Thread.Sleep(500);
             enlistment.Done();
         }
 
